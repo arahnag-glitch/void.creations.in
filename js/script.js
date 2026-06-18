43aaae924280788a29e0845978f946fa230d892d
arahnag-glitch <arahnag@gmail.com>
2026-06-18 14:56:17 +0530

    loader

--- js/script.js
+++ js/script.js
@@ -9,17 +9,10 @@ window.addEventListener("load", () => {
     const loader =
     document.getElementById("loader");
 
-    if(loader){
-
-        setTimeout(() => {
-
-            loader.style.opacity = "0";
-            loader.style.visibility = "hidden";
-
-        },4000);
-
+    if (loader) {
+        loader.style.opacity = "0";
+        loader.style.visibility = "hidden";
     }
-
 });
 
 
